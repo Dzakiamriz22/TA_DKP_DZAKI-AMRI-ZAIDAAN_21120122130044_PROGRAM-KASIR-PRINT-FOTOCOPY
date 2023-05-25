cc4bd4fabfd37f44aeffa2fae2ed9ada9cc4c2a0
Dzakiamriz22 <dzakiamriz12@gmail.com>
2023-05-25 21:33:13 +0700

    Update daftarHarga.py

--- daftarHarga.py
+++ daftarHarga.py
@@ -1,15 +1,33 @@
 class Harga:
     @staticmethod
     def get_harga_print_hitam_putih(ukuran):
-        harga_print_hitam_putih = {"A3": 750, "A4": 500, "F4": 500}
-        return harga_print_hitam_putih.get(ukuran, 0)
+        if ukuran == "A3":
+            return 500
+        elif ukuran == "A4":
+            return 300
+        elif ukuran == "F4":
+            return 400
+        else:
+            return 0
 
     @staticmethod
     def get_harga_print_berwarna(ukuran):
-        harga_print_berwarna = {"A3": 2000, "A4": 1000, "F4": 1500}
-        return harga_print_berwarna.get(ukuran, 0)
+        if ukuran == "A3":
+            return 1000
+        elif ukuran == "A4":
+            return 700
+        elif ukuran == "F4":
+            return 800
+        else:
+            return 0
 
     @staticmethod
     def get_harga_fotocopy(ukuran):
-        harga_fotocopy = {"A3": 500, "A4": 250, "F4": 250}
-        return harga_fotocopy.get(ukuran, 0)
+        if ukuran == "A3":
+            return 200
+        elif ukuran == "A4":
+            return 100
+        elif ukuran == "F4":
+            return 150
+        else:
+            return 0
